67c2f852ac69195770a96ebec910dff008dbbc31
Martin McCarthy <marty@theasis.co.uk>
2014-03-13 11:00:31 +0000

    style tweak for optional application form fields

--- app/helpers/photographers_helper.rb
+++ app/helpers/photographers_helper.rb
@@ -1,5 +1,5 @@
 module PhotographersHelper
   def optional(label)
-    sanitize(t(label)+" <span class='optional'>"+t(:application_form_label_optional_field)+"</span>")
+    sanitize(t(label)+" <span class='optional small'>"+t(:application_form_label_optional_field)+"</span>")
   end
 end
